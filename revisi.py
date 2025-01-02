7e70b6dd1e11bc8251df06efda89beeea29de291
hafidzfadillah23@gmail.com <hafidzfadillah23@gmail.com>
2025-01-02 12:07:06 +0700

    full commit

---
+++ revisi.py
@@ -0,0 +1,246 @@
+# library tampilan
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import scrolledtext
+# from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import os # baca folder, file
+import math # buat perhitungan
+from collections import Counter # counter/ngitung
+from docx import Document #baca file docx
+from PyPDF2 import PdfReader # baca file pdf
+import text_processor 
+
+class InformationRetrievalApp:
+    def __init__(self, master):
+        self.master = master
+        # create stemmer
+        # factory = StemmerFactory()
+        # self.stemmer = factory.create_stemmer()
+        self.text_processor = text_processor.TextProcessor()
+
+        self.directory_label = tk.Label(master, text="Select Directory:")
+        self.directory_label.pack()
+
+        # kolom nama folder
+        self.directory_var = tk.StringVar()
+        self.directory_entry = tk.Entry(master, textvariable=self.directory_var, width=50)
+        self.directory_entry.pack()
+
+        # tombol pilih folder
+        self.browse_button = tk.Button(master, text="Browse", command=self.browse_directory)
+        self.browse_button.pack()
+
+        self.query_label = tk.Label(master, text="Enter Query:")
+        self.query_label.pack()
+
+        # kolom input query
+        self.query_var = tk.StringVar()
+        self.query_entry = tk.Entry(master, textvariable=self.query_var, width=50)
+        self.query_entry.pack()
+
+        # tombol search
+        self.search_button = tk.Button(master, text="Search", command=self.perform_search)
+        self.search_button.pack()
+
+        self.files_label = tk.Label(master, text="List Dokumen:")
+        self.files_label.pack()
+
+        # kolom list dokumen pada folder yang dipilih
+        self.list_file = scrolledtext.ScrolledText(master, wrap=tk.WORD, width=70, height=10)
+        self.list_file.pack()
+
+        self.result_label = tk.Label(master, text="Hasil Pencarian  :")
+        self.result_label.pack()
+
+        # kolom hasil pencarian, diurutkan berdasarkan skor BM25 dari yang terbesar
+        self.result_text = scrolledtext.ScrolledText(master, wrap=tk.WORD, width=70, height=15)
+        self.result_text.pack()
+
+    # function untuk mengurutkan hasil pencarian berdasarkan skor BM25 dari yang terbesar
+    def sort_files_by_bm25_scores(self,scores):
+        return sorted(scores, key=lambda x: x[1], reverse=True)
+
+    def filter_document_terms(self, query_terms, document_terms):
+        return [term for term in document_terms if term in query_terms]
+
+    # function untuk proses perhitungan skor BM25
+    def calculate_bm25(self, query, document, document_length, average_document_length, k1=1.5, b=0.75):
+        # Splitting / memecah query dan document menjadi bentuk "terms"
+        print(f'query:{query}')
+        query_terms = self.text_processor.process_text(text=query)
+        document_terms = self.text_processor.process_text(text=document)
+        # document_terms = [self.perform_stemming(term) for term in document.lower().split()]
+        # document_terms = self.filter_document_terms(query_terms,doc_terms)
+
+        # Menghitung jumlah kemunculan setiap terms
+        term_freqs = Counter(document_terms)
+        
+        for word, count in sorted(term_freqs.items(), key=lambda x: x[1], reverse=True):
+            if count > 1:
+                print(f"Kata {word} sebanyak {count} kata\n")
+
+        # print(f'Term Frequency: {term_freqs}')
+        # print(f'Terms: {document_terms}')
+
+        # Proses perhitungan nilai IDF untuk setiap term pada query
+        # untuk mengukur seberapa jarang kemunculan suatu term pada dokumen
+        idf_values = {}
+        for term in query_terms:
+            df = sum(1 for doc_terms in document_terms if term in doc_terms)
+            idf_values[term] = math.log((len(document_terms) - df + 0.5) / (df + 0.5) + 1.0)
+
+        print(f'Nilai IDF: {idf_values}')
+
+        # Proses perhitungan skor BM25 untuk setiap term
+        bm25_scores = []
+        for term in query_terms:
+            tf = term_freqs[term]
+            numerator = tf * (k1 + 1)
+            denominator = tf + k1 * (1 - b + b * (document_length / average_document_length))
+            bm25_scores.append(idf_values[term] * numerator / denominator)
+
+        print(f'Skor BM25: {bm25_scores}')
+
+        # Mengembalikan jumlah skor BM25 untuk setiap term,
+        # yang mengukur relevansi antara query dan dokumen/file
+        return sum(bm25_scores)
+
+    # menghitung skor BM25 untuk masing masing file
+    def calculate_bm25_score(self, query, file, average_document_length, k1=1.5, b=0.75):
+        document_length = len(self.read_file(file).split())
+        print(f'\nNama File: {file}')
+        return self.calculate_bm25(query, self.read_file(file), document_length, average_document_length, k1, b)
+
+    # menampung hasil perhitungan skor BM25 untuk setiap file
+    def calculate_bm25_scores(self,query, files, average_document_length):
+        scores = [(file, self.calculate_bm25_score(query, file, average_document_length)) for file in files]
+        return scores
+
+    # def perform_stemming(self, text):
+    #     # return ' '.join(self.stemmer.stem(word) for word in text.split())
+    #     stemmed = self.stemmer.stem_kata(text)
+    #     return stemmed
+
+    def read_pdf(self,file_path):
+        # Membaca dan mengekstrak teks dari file PDF
+        try:
+            reader = PdfReader(file_path) 
+            text = ""
+            for page in reader.pages:
+                text += page.extract_text()
+            return text
+        except Exception as e:
+            print(f"Error membaca PDF {file_path}: {e}")
+            return ""
+
+    def read_docx(self,file_path):
+        # Membaca dan mengekstrak teks dari file DOCX
+        try:
+            doc = Document(file_path)
+            text = ""
+            for paragraph in doc.paragraphs:
+                text += paragraph.text + "\n"
+            return text
+        except Exception as e:
+            print(f"Error membaca DOCX {file_path}: {e}")
+            return ""
+
+    def read_txt(self,file_path):
+        # Membaca dan mengekstrak teks dari file TXT
+        try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                return file.read()
+        except Exception as e:
+            print(f"Error membaca TXT {file_path}: {e}")
+            return ""
+
+    # membaca dokumen berdasarkan ekstensi/jenis file
+    def read_file(self,file_path):
+        _, extension = os.path.splitext(file_path) # proses ambil ekstensi file
+
+        # jika jenis file adalah .txt
+        if extension == '.txt':
+            return self.read_txt(file_path)
+        # jika jenis file adalah .docx
+        elif extension == '.docx':
+            return self.read_docx(file_path)
+        # jika jenis file adalah PDF
+        elif extension == '.pdf':
+            return self.read_pdf(file_path)
+        else:
+            print('Ekstensi file tidak dikenal')
+            return ''
+
+    def calculate_average_document_length(self,files):
+        # hitung rata-rata panjang dokumen
+        total_words = sum(len(self.read_file(file).split()) for file in files)
+        return total_words / len(files)
+
+    def search_files_with_bm25(self,query, files):
+        # Hitung rata-rata panjang dokumen/file
+        average_document_length = self.calculate_average_document_length(files)
+        print(f'Rata-rata panjang dokumen: {average_document_length}')
+
+        # Hitung skor BM25
+        scores = self.calculate_bm25_scores(query, files, average_document_length)
+
+        # Urutkan file berdasarkan skor BM25 secara Descending
+        sorted_files = self.sort_files_by_bm25_scores(scores)
+
+        return sorted_files
+
+    def perform_search(self):
+        directory_path = self.directory_var.get() # ambil nama folder
+        query = self.query_var.get() # ambil nilai query
+
+        # cek apakah folder valid dan query tidak kosong
+        if not directory_path or not query:
+            self.result_text.delete(1.0, tk.END)
+            self.result_text.insert(tk.END, "Please select a directory and enter a query.")
+            return
+
+        # menampung semua file pada folder dalam suatu list
+        files = [os.path.join(directory_path, filename) for filename in os.listdir(directory_path) if
+                 os.path.isfile(os.path.join(directory_path, filename))]
+
+        self.list_file.insert(tk.END, f"Jumlah dokumen pada folder={len(files)}\n")
+        self.list_file.insert(tk.END, "========================================\n")
+
+        for rank, file in enumerate(files, start=1):
+            self.list_file.insert(tk.END, f"No.{rank} : {os.path.basename(file)}\n")
+
+        # print(f'Files in directory: {files}')
+        print(f'Search query: {self.query_var.get()}')
+
+        # Proses temu balik informasi
+        result_files = self.search_files_with_bm25(query, files)
+
+        # membersihkan kolom teks hasil
+        self.result_text.delete(1.0, tk.END)
+
+        # Jika terdapat file yang mengandung query, tampilkan
+        if result_files:
+            self.result_text.insert(tk.END, f"Menampilkan {len(result_files)} hasil\n")
+            self.result_text.insert(tk.END, f"=====================================\n")
+            for rank, (file, score) in enumerate(result_files, start=1):
+                if score > 0:
+                    self.result_text.insert(tk.END, f"Rank {rank}: {os.path.basename(file)} (Similarity Score: {score:.4f})\n")
+        # jika tidak
+        else:
+            self.result_text.insert(tk.END, "Tidak ada file yang cocok dengan query.")
+
+    # function untuk pilih directory/folder
+    def browse_directory(self):
+        print('Browse directory...')
+        selected_directory = filedialog.askdirectory()
+        print(f'Directory: {selected_directory}')
+        self.directory_var.set(selected_directory) # set nama folder ke kolom pertama
+
+# function utama
+def main():
+    root = tk.Tk()
+    app = InformationRetrievalApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ text_processor.py
@@ -0,0 +1,193 @@
+import re
+import nltk # Menggunakan nltk untuk tokenisasi teks
+from collections import Counter # Menggunakan Counter untuk menghitung frekuensi kata
+
+class TextProcessor:
+    def __init__(self):
+        self.kamus = self.load_kamus()
+        self.stopwords = self.get_stopwords()
+
+    def load_kamus(self):
+        # Memuat kamus kata dasar dari file files/kamuss.txt
+        try:
+            with open('files/kamuss.txt', 'r', encoding='utf-8') as file:
+                return {line.strip() for line in file if line.strip()}
+        except Exception as e:
+            print(f"Error membaca kamus: {e}")
+            return set()
+
+    def get_stopwords(self):
+        # Memuat daftar stopword dari file dan mengembalikan dalam bentuk set
+        try:
+            with open('files/stopwordbahasa.xls', 'r', encoding='utf-8') as file:
+                stopwords = {line.strip() for line in file if line.strip()}
+            return stopwords
+        except Exception as e:
+            print(f"Error membaca file stopwords: {e}")
+            return set()
+
+    
+    def clean_text(self, text):
+        content = text.lower()
+        content = ' '.join(content.split())  # Menghapus spasi berlebih
+        content = re.sub(r'http\S+|www\S+|\.|\,', '', content) # Menghapus URL, HTTP, koma, dan titik
+        content = re.sub(r'[^a-zA-Z\s]', '', content)
+        return content
+    
+    def tokenizing(self,text):
+        try:
+            tokens = nltk.word_tokenize(text)
+            return tokens
+        except Exception as e:
+            print(f"Error dalam tokenisasi: {e}")
+            return []
+    
+    def stopword_removal(self, tokens):
+        """Remove stopwords from token list"""
+        if not isinstance(tokens, list):
+            print('oops')
+            return []
+        return [token for token in tokens if token not in self.stopwords]
+
+    def cek_kata_dasar(self,kata):
+        # Mengecek apakah kata ada dalam kamus kata dasar
+        if self.kamus is None:
+            self.kamus = self.load_kamus()
+        return kata in self.kamus
+
+    def hapus_inflection_suffixes(self, kata):
+        # Menghapus inflectional suffixes (-lah, -kah, -ku, -mu, atau -nya)
+        if kata.endswith(('lah', 'kah')):
+            return kata[:-3]
+        if kata.endswith(('ku', 'mu')):
+            return kata[:-2] 
+        if kata.endswith('nya'):
+            return kata[:-3] 
+        if kata.endswith('tah'):
+            return kata[:-3]
+        if kata.endswith('pun'):
+            return kata[:-3]
+        return kata
+
+    def hapus_derivation_suffixes(self, kata):
+        # Menghapus derivational suffixes (-i, -an, atau -kan)
+        if kata.endswith('i'):
+            return kata[:-1]
+        if kata.endswith('an'):
+            return kata[:-2]
+        if kata.endswith('kan'):
+            return kata[:-3]
+        return kata
+
+    def hapus_derivation_prefix(self, kata):
+        # Menghapus derivational prefixes (di-, ke-, se-, me-, be-, pe-, te-)
+        if len(kata) <= 4:
+            return kata
+            
+        # Cek awalan di- dan ke-
+        if kata.startswith(('di', 'ke')) and len(kata) > 4:
+            kata_tanpa_awalan = kata[2:]
+            if self.cek_kata_dasar(kata_tanpa_awalan):
+                return kata_tanpa_awalan
+                
+        # Cek awalan se-    
+        if kata.startswith('se'):
+            kata_tanpa_awalan = kata[2:]
+            if self.cek_kata_dasar(kata_tanpa_awalan):
+                return kata_tanpa_awalan
+                
+        # Cek awalan me-
+        if kata.startswith('me'):
+            kata_tanpa_awalan = kata[2:]
+            if kata.startswith('meng'):
+                kata_tanpa_awalan = kata[4:]
+            elif kata.startswith('meny'):
+                kata_tanpa_awalan = 's' + kata[4:]
+            elif kata.startswith('men'):
+                kata_tanpa_awalan = kata[3:]
+            elif kata.startswith('mem'):
+                kata_tanpa_awalan = 'p' + kata[3:]
+            if self.cek_kata_dasar(kata_tanpa_awalan):
+                return kata_tanpa_awalan
+                
+        # Cek awalan be-
+        if kata.startswith('be'):
+            kata_tanpa_awalan = kata[2:]
+            if kata.startswith('ber'):
+                kata_tanpa_awalan = kata[3:]
+            if self.cek_kata_dasar(kata_tanpa_awalan):
+                return kata_tanpa_awalan
+                
+        # Cek awalan pe-
+        if kata.startswith('pe'):
+            kata_tanpa_awalan = kata[2:]
+            if kata.startswith('peng'):
+                kata_tanpa_awalan = kata[4:]
+            elif kata.startswith('peny'):
+                kata_tanpa_awalan = 's' + kata[4:]
+            elif kata.startswith('pen'):
+                kata_tanpa_awalan = kata[3:]
+            elif kata.startswith('pem'):
+                kata_tanpa_awalan = 'p' + kata[3:]
+            if self.cek_kata_dasar(kata_tanpa_awalan):
+                return kata_tanpa_awalan
+                
+        # Cek awalan te-
+        if kata.startswith('te'):
+            kata_tanpa_awalan = kata[2:]
+            if kata.startswith('ter'):
+                kata_tanpa_awalan = kata[3:]
+            if self.cek_kata_dasar(kata_tanpa_awalan):
+                return kata_tanpa_awalan
+                
+        return kata
+
+    def stem_kata(self, kata):
+        # Implementasi algoritma Nazief-Adriani
+        if self.cek_kata_dasar(kata):
+            return kata
+            
+        # Hapus inflectional suffixes
+        kata_1 = self.hapus_inflection_suffixes(kata)
+        if self.cek_kata_dasar(kata_1):
+            return kata_1
+            
+        # Hapus derivational suffix
+        kata_2 = self.hapus_derivation_suffixes(kata_1)
+        if self.cek_kata_dasar(kata_2):
+            return kata_2
+            
+        # Hapus derivational prefix
+        kata_3 = self.hapus_derivation_prefix(kata_2)
+        if self.cek_kata_dasar(kata_3):
+            return kata_3
+            
+        return kata
+
+    def stem_text(self, text):
+        # Memisahkan teks menjadi kata-kata
+        kata_kata = text.split()
+        
+        # Melakukan stemming untuk setiap kata
+        hasil_stemming = []
+        for kata in kata_kata:
+            kata_dasar = self.stem_kata(kata)
+            hasil_stemming.append(kata_dasar)
+            
+        # Menggabungkan kembali kata-kata yang telah di-stemming
+        return ' '.join(hasil_stemming)
+    
+    def process_text(self, text):
+        content = self.clean_text(text)
+        tokens = self.tokenizing(content)
+        filtered_tokens = self.stopword_removal(tokens)
+        stemmed_tokens = [self.stem_kata(token) for token in filtered_tokens] 
+        print(str(stemmed_tokens))
+
+        # return ' '.join(stemmed_tokens)
+        return stemmed_tokens
+
+        # word_freq = Counter(stemmed_tokens)
+        # print("\n\n=== Word Frequencies ===\n")
+        # for word, count in sorted(word_freq.items(), key=lambda x: x[1], reverse=True):
+        #     print(f"Kata {word} sebanyak {count} kata\n")
